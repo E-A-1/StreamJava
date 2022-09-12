07b732176661f7d98d9e0668ee41589177d0909c
EA Vijay <elijaarulin.vijayakumar@devaxcel.com>
2022-09-12 23:54:11 +0530

    understanding recursion WIP

--- src/JavaRecursion.java
+++ src/JavaRecursion.java
@@ -1,4 +1,4 @@
-public class JavaRecursion {
+public class JavaRecursion implements RecursionPractice {
 
     static int count=0;
 
@@ -10,8 +10,59 @@ public class JavaRecursion {
 printNumber();
         }
     }
+    static int factorial(int n){     
+        System.out.println("the factorial function called"); 
+        System.out.println("the input number is "+n); 
+        System.out.println("the calculated value till now is "+count);
+        if (n == 1)   
+        {
+            return 1;      
+
+        }   
+        else      {
+            System.out.println("before"+count);
+
+            count= (n * factorial(n-1));    
+            System.out.println("after"+count);
+            return count;  
+
+        }
+  }      
+
     public static void main(String[] args) {
-        printNumber();
+        JavaRecursion javaRecursion=new JavaRecursion();
+        javaRecursion.getFactorialNumberWithoutRecursion(6);
+        int factorialValue=factorial(5);
+        System.out.println("the factorial value is "+factorialValue);
     }
+    /*
+
+    Find the factorial of a number using recursion
+     * 
+     */
+    @Override
+    public  void getFactorialNumberWithoutRecursion(int number) {
+        int inputNumber=number;
+        int factorialValue=1;
+        if(number>0){
+
+for(int factNumber=number;factNumber>0;factNumber--){
+    factorialValue=factorialValue*factNumber;
+    number--;
+}
+System.out.println("the factorial of the number "+inputNumber+" is "+factorialValue);
+        }
+    }
+    @Override
+    public void getFactorialNumberWithRecursion(int number) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+
+    
+
+
+
     
 }
---
+++ src/RecursionPractice.java
@@ -0,0 +1,4 @@
+public interface RecursionPractice {
+    void getFactorialNumberWithoutRecursion(int number);
+    void getFactorialNumberWithRecursion(int number);
+}
