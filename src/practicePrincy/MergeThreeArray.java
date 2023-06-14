8e0ccb8eb4612faa7ef83bd902049e2ab109245e
EAVijay <elijaarulin.vijayakumar@troniqsrationale.com>
2023-06-14 23:29:59 +0530

    added substring

--- src/practicePrincy/MergeThreeArray.java
+++ src/practicePrincy/MergeThreeArray.java
@@ -1,30 +1,34 @@
 package practicePrincy;
 
 public class MergeThreeArray {
-
+    // Recommended
     public static void mergeArrayBruteForce() {
         int x[] = { 1, 2, 3 };
         int y[] = { 4, 5, 6 };
         int z[] = { 7, 8, 9 };
 
         int resultArray[] = new int[x.length + y.length + z.length];
-
+        // Transferring first arrays element to resultArray
         for (int i = 0; i < x.length; i++) {
             resultArray[i] = x[i];
         }
+
+        // Transferring second arrays element to resultArray
 
         for (int i = 0; i < y.length; i++) {
             resultArray[x.length + i] = y[i];
 
         }
 
+        // Transferring third arrays element to resultArray
+
         for (int i = 0; i < z.length; i++) {
             resultArray[x.length + y.length + i] = z[i];
 
         }
 
-        for (int value : resultArray) {
-            System.out.print(value + " ");
+        for (int i = 0; i < resultArray.length; i++) {
+            System.out.print(resultArray[i] + " ");
         }
     }
 
@@ -37,7 +41,9 @@ public class MergeThreeArray {
         int[] mergedArray = new int[totalLength];
 
         System.arraycopy(array1, 0, mergedArray, 0, array1.length);
+
         System.arraycopy(array2, 0, mergedArray, array1.length, array2.length);
+
         System.arraycopy(array3, 0, mergedArray, array1.length + array2.length, array3.length);
 
         // Output the merged array
@@ -47,6 +53,6 @@ public class MergeThreeArray {
     }
 
     public static void main(String[] args) {
-        mergeArrayUsingInBuilt();
+        mergeArrayBruteForce();
     }
 }
--- src/practicePrincy/SubString.java
+++ src/practicePrincy/SubString.java
@@ -2,6 +2,18 @@ package practicePrincy;
 
 public class SubString {
     public static String reverseString(String inputValue) {
+        // Converting a string to charArray
+        char[] inputCharArray = inputValue.toCharArray();
+        String result = "";
+        for (int i = inputCharArray.length - 1; i >= 0; i--) {
+            result += inputCharArray[i];
+        }
+
+        return result;
+    }
+
+    public static String subString(String inputValue) {
+        // Converting a string to charArray
         char[] inputCharArray = inputValue.toCharArray();
         String result = "";
         for (int i = inputCharArray.length - 1; i >= 0; i--) {
@@ -12,7 +24,9 @@ public class SubString {
     }
 
     public static void main(String[] args) {
-        String inputValue = "Welcome";
+        String inputValue = "Princy";
         System.out.println(SubString.reverseString(inputValue));
+
+        System.out.println(inputValue.substring(0, 6));
     }
 }
